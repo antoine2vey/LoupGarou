99be279d220deb42d412ff2fe300ba172bc0a66b
Thibault <thibault.carlier02@gmail.com>
2017-04-12 11:47:14 +0200

    CorrectionPopLogin Et Installation Route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule, Pipe} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
 import { ChatService } from './chat/chat.service';
 import { LoginComponent } from './login/login.component';
 import { LoginService } from './login.service';
 import { UsersComponent } from './users/users.component';
+import { routes } from './app.router';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatComponent,
     LoginComponent,
-    UsersComponent
+    UsersComponent,
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routes
   ],
   providers: [LoginService, ChatService],
   bootstrap: [AppComponent]
